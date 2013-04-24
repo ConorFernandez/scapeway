aa9a64033f4eded63d3625ecc1508b646c466db1
Conor Fernandez <conor.fernandez@gmail.com>
2013-04-24 16:21:55 -0700

    Edit application.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,4 @@ Scapeway::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
-  config.assets.precompile += ['static_pages.css']
 end
