90180d71721c1f0c0d69dadb0861c04d7380ba64
Conor Fernandez <conor.fernandez@gmail.com>
2013-04-24 14:54:47 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scapeway::Application.config.secret_key_base = 'ba21be11335c7460ebb2d3acb36f93440629bd3143bfb65917ac8161b497ef561a4fa1f7185646ae2363cabd0757b924b5795ad82a55791980295f98687daa10'
